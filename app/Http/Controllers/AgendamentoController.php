bc1463cbd6205319178be5a9fab248bfdac344fa
andrecavichiolli3@gmail.com <128004877+PetiteSneeze@users.noreply.github.com>
2024-10-08 12:20:49 -0300

    AgendamentoController

---
+++ app/Http/Controllers/AgendamentoController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Agendamento;
+use Illuminate\Http\Request;
+use App\Models\AvaliacaoPsicologica;
+use App\Models\Paciente;
+use Illuminate\Support\Facades\Auth;
+
+class AgendamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $userId = Auth::id();
+        $paciente = Paciente::where('user_id', $userId)->get();
+        $pacientesId = $paciente->pluck("id");
+        $agendamento = Agendamento::whereIn('agendamento', $paciente)->get();
+        return view('agendamento.index', compact('agendamento', 'pacientesId'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $userId = Auth::id();
+        $pacientes = Paciente::where('user_id', $userId)->get();
+        return view('agendamento.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Agendamento::create($request->all());
+        return redirect()->route('agendamento.index')
+                                ->with('insercao','Inserido com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $agendamento = Agendamento::findOrfail($id);
+        return view('agendamento.show', compact('agendamento'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $agendamento = Agendamento::findOrfail($id);
+        return view('agendamento.edit', compact('agendamento'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $agendamento = Agendamento::findOrfail($id);
+        $agendamento->update($request->all());
+        return redirect()->route('agendamento.index')
+                                ->with('atualizacao','Atualizado com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
