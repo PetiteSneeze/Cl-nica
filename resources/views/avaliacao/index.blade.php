5f87226cb17e114054ad67680aab531cae68b86d
andrecavichiolli3@gmail.com <128004877+PetiteSneeze@users.noreply.github.com>
2024-10-09 21:33:23 -0300

    avaliação view

---
+++ resources/views/avaliacao/index.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+
+    <h2>Gerenciar avaliações psicológicas</h2>
+    <a href="/avaliacao/create" class="btn btn-primary">
+        Novo Registro
+    </a>
+    <table class="table table-hover">
+        
+    <thead>
+        <tr>
+            <th>Paciente</th>
+            <th>Data da Avaliação</th>
+            <th>Observações</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach $avaliacoes as $a
+            <tr>
+                <td>{{ $a->data_avaliacao}}</td>
+                <td>{{ $a->paciente->nome}}</td>
+                <td>
+                    <a href="/avaliacao/{{$a->id}}">
+                        Visualizar registro
+                    </a>
+                    <a href="/avaliacao/{{ $a->id }}/edit" class="btn btn-warning">
+                        Editar
+                    </a>
+                    
+                </td>
+            </tr>
+        @endforeach
+</x-app-layout>
